2abb1e978df5174e89e5d6dc0b17d1d4449e1e37
BillyPittman1993 <billytrading93@gmail.com>
2017-12-11 01:19:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+  array.push(musicians[i] + " plays " + instruments[i])
+}
+return array
+}
+
+function johnLennonFacts(facts) {
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  var newFacts = [];
+  let i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + '!!!')
+    i++
+  }
+  return newFacts
+}
